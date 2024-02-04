552dfca66c8478731b61005f675aeb546be6c15c
Katlego-Sekoele <sekoelekatlego@gmail.com>
2024-02-04 20:14:40 +0200

    changed to arrow function

--- election-platform-backend/src/index.js
+++ election-platform-backend/src/index.js
@@ -1,13 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-})
-const PORT = 3001;
- 
-app.listen(PORT, () => {
-    console.log(`Running on PORT ${PORT}`);
-})
+const connect = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_URI);
+
+		console.info("Connected to MongoDB");
+		app.listen(PORT, () => {
+			console.info(`Running on http://127.0.0.1:${PORT}`);
+		});
+	} catch (error) {
+		console.error("Error connecting to MongoDB", error);
+	}
+};
+
+app.use(express.json());
+app.use("/", indexRoutes);
+
+connect();
 
 module.exports = app;
