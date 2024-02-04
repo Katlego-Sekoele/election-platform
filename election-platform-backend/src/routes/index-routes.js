5f025d823c8de2173ae44250e3c6c578ff1cae4e
Katlego-Sekoele <sekoelekatlego@gmail.com>
2024-02-04 20:06:28 +0200

    set up mongodb atlas connection

--- election-platform-backend/src/routes/index-routes.js
+++ election-platform-backend/src/routes/index-routes.js
@@ -1,14 +1,8 @@
 const express = require('express');
-const UserModel = require('../models/user');
 const router = express.Router();
 
-router.get('/hello-world', async (request, response) => {
-  try {
-    response.send(`Hello, World! Time: ${Date.now().toString()}`);
-  } catch (error) {
-    console.error(error);
-    response.status(500).json({ error: error });
-  }
+router.get('/hello-world', (req, res) => {
+  res.send(`Hello, World! Time: ${Date.now().toString()}`);
 });
 
 module.exports = router;
